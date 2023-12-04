c4e573d41b51c478821dedd03f82dd4ac5ab1a82
HC-kang <weston0713@gmail.com>
2023-12-04 22:53:16 +0900

    Feat: add calculator

---
+++ src/calculator.js
@@ -0,0 +1,32 @@
+class Calculator {
+  constructor() {
+    this.value = 0;
+  }
+
+  set(num) {
+    this.value = num;
+  }
+
+  clear() {
+    this.value = 0;
+  }
+
+  add(num) {
+    this.value += num;
+  }
+
+  subtract(num) {
+    this.value -= num;
+  }
+
+  multiply(num) {
+    this.value *= num;
+  }
+
+  divide(num) {
+    if (num === 0) throw new Error('Cannot divide by 0');
+    this.value /= num;
+  }
+}
+
+module.exports = Calculator;
