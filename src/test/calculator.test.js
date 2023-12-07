19e303d8a918a7f7a5d554add0099c4848691ee6
HC-kang <weston0713@gmail.com>
2023-12-07 15:53:59 +0900

    Test: modify test for calculator

--- src/test/calculator.test.js
+++ src/test/calculator.test.js
@@ -1,67 +1,67 @@
 const Calculator = require('../calculator.js');
 
 describe('Calculator', () => {
+  let cal;
+  beforeEach(() => {
+    cal = new Calculator();
+  })
+  it('inits with 0', () => {
+    expect(cal.value).toBe(0);
+  });
+
   describe('Set', () => {
     it('should set the value', () => {
-      const calculator = new Calculator();
-      calculator.set(5);
-      expect(calculator.value).toBe(5);
+      cal.set(5);
+      expect(cal.value).toBe(5);
     });
 
     it('should set the value to 0 if no value is passed', () => {
-      const calculator = new Calculator();
-      expect(calculator.value).toBe(0);
+      expect(cal.value).toBe(0);
     });
   });
 
   describe('Clear', () => {
     it('should set the value to 0', () => {
-      const calculator = new Calculator();
-      calculator.set(5);
-      calculator.clear();
-      expect(calculator.value).toBe(0);
+      cal.set(5);
+      cal.clear();
+      expect(cal.value).toBe(0);
     });
   });
 
   describe('Add', () => {
     it('should add the value', () => {
-      const calculator = new Calculator();
-      calculator.set(5);
-      calculator.add(5);
-      expect(calculator.value).toBe(10);
+      cal.set(5);
+      cal.add(5);
+      expect(cal.value).toBe(10);
     });
   });
 
   describe('Subtract', () => {
     it('should subtract the value', () => {
-      const calculator = new Calculator();
-      calculator.set(5);
-      calculator.subtract(5);
-      expect(calculator.value).toBe(0);
+      cal.set(5);
+      cal.subtract(5);
+      expect(cal.value).toBe(0);
     });
   });
 
   describe('Multiply', () => {
     it('should multiply the value', () => {
-      const calculator = new Calculator();
-      calculator.set(5);
-      calculator.multiply(5);
-      expect(calculator.value).toBe(25);
+      cal.set(5);
+      cal.multiply(5);
+      expect(cal.value).toBe(25);
     });
   });
 
   describe('Divide', () => {
     it('should divide the value', () => {
-      const calculator = new Calculator();
-      calculator.set(5);
-      calculator.divide(5);
-      expect(calculator.value).toBe(1);
+      cal.set(5);
+      cal.divide(5);
+      expect(cal.value).toBe(1);
     });
 
     it('should throw an error if dividing by 0', () => {
-      const calculator = new Calculator();
-      calculator.set(5);
-      expect(() => calculator.divide(0)).toThrow('Cannot divide by 0');
+      cal.set(5);
+      expect(() => cal.divide(0)).toThrow('Cannot divide by 0');
     });
   });
 });
